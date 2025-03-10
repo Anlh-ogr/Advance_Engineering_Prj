6a0735a85906fedb216ddc5199a70360e74e8203
Anlh-ogr <9.10hoangan.13072003@gmail.com>
2025-03-10 16:23:47 +0700

    Create Gamepad_Pub.py

---
+++ Week_06/Three-Node/Gamepad_Pub.py
@@ -0,0 +1,35 @@
+import json
+import rclpy                    # type: ignore
+from rclpy.node import Node     # type: ignore
+from std_msgs.msg import String # type: ignore
+from inputs import get_gamepad  # type: ignore
+
+class GamepadDataPublisher(Node):
+    def __init__(self):
+        super().__init__('gamepad_data_publisher')  # Node name
+        
+        # Create a publisher
+        self.publisher_ = self.create_publisher(String, 'gamepad_data', 10)
+        timer_period = 0.001
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        
+    def timer_callback(self):
+        events = get_gamepad()
+        for event in events:
+            if event.ev_type in ["Key", "Absolute"]:
+                if event.code in ["ABS_Y", "ABS_X", "ABS_Z", "ABS_RZ", "BTN_TL", "BTN_TR"]:
+                    msg = String()
+                    data = {event.code: event.state} # Create a dictionary
+                    msg.data = json.dumps(data)      # Convert to JSON
+                    self.publisher_.publish(msg)
+                    self.get_logger().info(f'Publishing: "{msg.data}"')
+                    
+def main(args=None):
+    rclpy.init(args=args)
+    gamepad_data_publisher = GamepadDataPublisher()
+    rclpy.spin(gamepad_data_publisher)
+    gamepad_data_publisher.destroy_node()
+    rclpy.shutdown()
+    
+if __name__ == '__main__':
+    main()
